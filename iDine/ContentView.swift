5f260312a26aa85121b171c9c714fd7c298c6639
abhiaher22 <abhiaher22@gmail.com>
2020-01-11 19:34:53 -0800

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Abhijeet Aher on 1/11/20.
+//  Copyright © 2020 Abhijeet Aher. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
